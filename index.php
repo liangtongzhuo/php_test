89b4f838816c734d5f76726e70f95a0a142036de
ltz <liangtongzhuo@gmail.com>
2018-10-17 15:59:23 +0800

    success login register

--- index.php
+++ index.php
@@ -5,18 +5,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>PHP testing</title>
+</head>
 
 <body>
     <h1>home</h1>
-
     <form action="login.php" method="post">
-        Name: <input type="text" name="username"><br>
-        password: <input type="text" name="password"><br>
-        <input type="submit" name="submit" value="login">
-        <input type="submit" name="submit" value="register">
+            Name: <input type="text" name="username"><br>
+            password: <input type="text" name="password"><br>
+            <input type="submit" name="submit" value="login">
+            <input type="submit" name="submit" value="register">
     </form>
-
     <?php
+    
         $mysqli = new mysqli('127.0.0.1', 'root', '123456', 'testing', 33060);
         $result = $mysqli->query("SELECT username FROM users");
         if (mysqli_connect_error()) {
--- login.php
+++ login.php
@@ -5,32 +5,71 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>login/register</title>
-
 <body>
 <?php
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        $submit = $_POST['submit'];
-        echo '<h1>'.$submit.'</h1>';
-        $mysqli = new mysqli('127.0.0.1', 'root', '123456', 'testing', 33060);
-        // print_r($mysqli);
-        $result = $mysqli->query("SELECT password FROM users WHERE username = "."'$username'");
-        if (mysqli_connect_error()) {
-            echo mysqli_connect_error();
-        }
-        $rs=$result->fetch_row();
-        if ($rs!==null) {
-            if ($password == $rs[0]) {
-                $arr = array('status' => 1, 'b' => 'success');
-                echo json_encode($arr);
-            } else {
-                $arr = array('status' => 2, 'b' => 'password error');
-                echo json_encode($arr);
-            }
+echo '</br></br> <a href="./">back Home</a> </br> ';
+
+init();
+function init()
+{
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    if (!$username) {
+        echo 'username not empty';
+        return;
+    }
+    if (!$password) {
+        echo 'password not empty';
+        return ;
+    }
+
+    $submit = $_POST['submit'];
+    echo '<h1>'.$submit.'</h1>';
+    $mysqli = new mysqli('127.0.0.1', 'root', '123456', 'testing', 33060);
+    $result = $mysqli->query("SELECT password FROM users WHERE username = '{$username}'");
+    if (mysqli_connect_error()) {
+        echo mysqli_connect_error();
+    }
+    $rs=$result->fetch_row();
+    if ($submit == 'login') {
+        login($rs, $username, $password);
+    } else {
+        register($rs, $username, $password);
+    }
+}
+
+    
+function login($rs, $username, $password)
+{
+    if ($rs!==null) {
+        if ($password == $rs[0]) {
+            echo "login success";
+            setcookie("login", $username);
         } else {
-            $arr = array('status' => 0, 'b' => 'not username');
-            echo json_encode($arr);
+            echo "login password error";
         }
+    } else {
+        echo "login not userName";
+    }
+}
+
+function register($rs, $username, $password)
+{
+    if ($rs[0]) {
+        echo "The account already exists";
+        return;
+    }
+    $mysqli = new mysqli('127.0.0.1', 'root', '123456', 'testing', 33060);
+    $result = $mysqli->query("INSERT INTO users (username, password) VALUES ('{$username}', '{$password}')");
+    if ($result) {
+        echo "register success";
+        setcookie("login", $username);
+    } else {
+        echo"register error";
+    }
+}
+
 ?>
 </body>
+
 </html>
